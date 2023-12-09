575a676e1c85c3f6cb1d5f61ace1aeb26706bd98
Oliver Smith <siroliversmith@outlook.com>
2023-12-09 21:04:40 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const { Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.username} is online`);
+})
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    
+    if (interaction.commandName === 'hey') {
+        interaction.reply('Greetings, how may I assist you?');
+    }
+
+    if (interaction.commandName === 'ping') {
+        interaction.reply('Pong!');
+    }
+
+    if (interaction.commandName === 'add') {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+        
+        interaction.reply(`The sum is ${num1 + num2}`);
+    }
+});
+
+client.login(process.env.TOKEN);
---
+++ src/register-commands.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js')
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Greets the user' 
+     
+    },
+    {
+      name: 'ping',
+      description: 'Replies with pong!' 
+   
+  },
+  {
+    name: 'add',
+    description: 'Adds two numbers together',
+    options: [
+      {
+        name: 'first-number',
+        description: 'The first number to add',
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+      },
+      {
+        name: 'second-number',
+        description: 'The second number to add',
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+      }
+    ]
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+    
+(async () => {
+  try {
+    console.log('Registering slash commands...');
+    
+    await rest.put(
+      Routes.applicationGuildCommands (
+        process.env. CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+    console.log('Slash commands were registered successfully!');
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
+})();
