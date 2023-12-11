179eaa7ff36fe4482e6110ae06ed830f5cf1e548
Oliver Smith <siroliversmith@outlook.com>
2023-12-11 19:41:28 +0000

    temp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 require('dotenv').config();
-const { Client, IntentsBitField} = require('discord.js');
+const { Client, IntentsBitField, SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const puppeteer = require('puppeteer');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('chatgpt')
+    .setDescription('ask ChatGPT a question')
+    .addStringOption(option => option.setName('prompt').setDescription('The prompt for the ai').setRequired(true)),
+    async execute(interaction) {
+
+        await interaction.reply({content: `Thinking...`, ephemeral: true});
+
+        const { options } = interaction;
+        const prompt = options.getString('prompt');
+
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        await page.goto('https://chat-app-f2d296.zapier.app/');
+
+        const textBoxSelector = 'textarea[aria-label="chatbot-user-prompt"]';
+        await page.waitForSelector(textBoxSelector);
+        await page.type(textBoxSelector, prompt);
+
+        await page.keyboard.press('Enter');
+
+        await page.waitForSelector('[data-testid="final-bot-response"] p');
+
+        var value = await page.$$eval('[data-testid="final-bot-response"]', async (element) => {
+            return elements.map((element) => element.textContent);
+        });
+
+        setTimeout(async () => {
+            if (value.length == 0) return await interaction.editReply({content: `I'm sorry, I couldn't find a response to your question.`, ephemeral: true});
+        }, 30000)
+
+        await browser.close();
+
+        value.shift();
+        const embed = new EmbedBuilder()
+        .setColor("Blurple")
+        .setDescription(`\`\`\`${value.join(`\n\n\n\n`)}\`\`\``);
+
+        await interaction.editReply({content: '', embeds: [embed] });
+    }
+}
 
 const client = new Client({
     intents: [
@@ -13,24 +58,5 @@ const client = new Client({
 client.on('ready', (c) => {
     console.log(`✅ ${c.user.username} is online`);
 })
-
-client.on('interactionCreate', (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    if (interaction.commandName === 'hey') {
-        interaction.reply('Greetings, how may I assist you?');
-    }
-
-    if (interaction.commandName === 'ping') {
-        interaction.reply('Pong!');
-    }
-
-    if (interaction.commandName === 'add') {
-        const num1 = interaction.options.get('first-number').value;
-        const num2 = interaction.options.get('second-number').value;
-        
-        interaction.reply(`The sum is ${num1 + num2}`);
-    }
-});
 
 client.login(process.env.TOKEN);
