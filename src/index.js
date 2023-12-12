ce815228b34b20fc591fa9ab0f1e3801f77a25e0
Oliver Smith <123121646+Batmxn@users.noreply.github.com>
2023-12-12 12:37:13 +0000

    push

--- src/index.js
+++ src/index.js
@@ -2,50 +2,6 @@ require('dotenv').config();
 const { Client, IntentsBitField, SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 const puppeteer = require('puppeteer');
 
-module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('chatgpt')
-    .setDescription('ask ChatGPT a question')
-    .addStringOption(option => option.setName('prompt').setDescription('The prompt for the ai').setRequired(true)),
-    async execute(interaction) {
-
-        await interaction.reply({content: `Thinking...`, ephemeral: true});
-
-        const { options } = interaction;
-        const prompt = options.getString('prompt');
-
-        const browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-
-        await page.goto('https://chat-app-f2d296.zapier.app/');
-
-        const textBoxSelector = 'textarea[aria-label="chatbot-user-prompt"]';
-        await page.waitForSelector(textBoxSelector);
-        await page.type(textBoxSelector, prompt);
-
-        await page.keyboard.press('Enter');
-
-        await page.waitForSelector('[data-testid="final-bot-response"] p');
-
-        var value = await page.$$eval('[data-testid="final-bot-response"]', async (element) => {
-            return elements.map((element) => element.textContent);
-        });
-
-        setTimeout(async () => {
-            if (value.length == 0) return await interaction.editReply({content: `I'm sorry, I couldn't find a response to your question.`, ephemeral: true});
-        }, 30000)
-
-        await browser.close();
-
-        value.shift();
-        const embed = new EmbedBuilder()
-        .setColor("Blurple")
-        .setDescription(`\`\`\`${value.join(`\n\n\n\n`)}\`\`\``);
-
-        await interaction.editReply({content: '', embeds: [embed] });
-    }
-}
-
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -54,6 +10,58 @@ const client = new Client({
         IntentsBitField.Flags.MessageContent,
     ]
 });
+
+const PREFIX = '!';
+const XP_PER_MESSAGE = 100;
+
+const userXP = {};
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('message', (message) => {
+    if (message.author.bot) return;
+
+    // Increment user XP
+    const userId = message.author.id;
+    userXP[userId] = (userXP[userId] || 0) + XP_PER_MESSAGE;
+
+    // Check for level-up
+    const userLevel = Math.floor(userXP[userId] / 100);
+    if (userLevel > 0) {
+        message.channel.send(`${message.author.username}, you leveled up to level ${userLevel}!`);
+    }
+
+    // Check for commands
+    if (message.content.startsWith(PREFIX)) {
+        const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        // Add more commands if needed
+        if (command === 'level') {
+            message.channel.send(`You are currently level ${userLevel}.`);
+        }
+    }
+});
+
+registerCommands(client);
+
+client.on('message', (message) => {
+    if (message.author.bot) return;
+
+    // Increment user XP
+    const userId = message.author.id;
+    userXP[userId] = (userXP[userId] || 0) + XP_PER_MESSAGE;
+
+    // Check for level-up
+    const userLevel = Math.floor(userXP[userId] / 100);
+    if (userLevel > 0) {
+        message.channel.send(`${message.author.username}, you leveled up to level ${userLevel}!`);
+        // You can add additional actions when a user levels up
+    }
+});
+
 
 client.on('ready', (c) => {
     console.log(`✅ ${c.user.username} is online`);
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,36 +1,31 @@
 require('dotenv').config();
 const { REST, Routes, ApplicationCommandOptionType } = require('discord.js')
 
-const commands = [
-    {
-        name: 'hey',
-        description: 'Greets the user' 
-     
-    },
-    {
-      name: 'ping',
-      description: 'Replies with pong!' 
-   
-  },
-  {
-    name: 'add',
-    description: 'Adds two numbers together',
-    options: [
-      {
-        name: 'first-number',
-        description: 'The first number to add',
-        type: ApplicationCommandOptionType.Number,
-        required: true,
-      },
-      {
-        name: 'second-number',
-        description: 'The second number to add',
-        type: ApplicationCommandOptionType.Number,
-        required: true,
+function registerCommands(client) {
+  const PREFIX = '!';
+
+  client.on('message', (message) => {
+      if (message.author.bot || !message.content.startsWith(PREFIX)) return;
+
+      const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+      const command = args.shift().toLowerCase();
+
+      if (command === 'level') {
+          checkLevel(message);
       }
-    ]
-  }
-];
+      // Add more commands here if needed
+  });
+}
+
+function checkLevel(message) {
+  const userId = message.author.id;
+  const userXP = userXP[userId] || 0;
+  const userLevel = Math.floor(userXP / 100);
+
+  message.channel.send(`${message.author.username}, you are currently level ${userLevel}.`);
+}
+
+module.exports = registerCommands;
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
     
