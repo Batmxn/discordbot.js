14cd9757137daba3c429790a9aa9ef4f23e815c8
Oliver Smith <siroliversmith@outlook.com>
2023-12-12 22:21:32 +0000

    Slash Commands

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const { Client, IntentsBitField, SlashCommandBuilder, EmbedBuilder} = require('discord.js');
-const puppeteer = require('puppeteer');
+const { Client, IntentsBitField, ActivityType, EmbedBuilder } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -8,63 +7,144 @@ const client = new Client({
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
-    ]
+    ],
 });
 
-const PREFIX = '!';
-const XP_PER_MESSAGE = 100;
+const alfredGreetings = [
+        "Good day, sir. How may I be of service?",
+        "Welcome back, master. Is there anything I can assist you with?",
+        "Greetings, sir. I trust your endeavors are proceeding smoothly.",
+        "Ah, it's a pleasure to see you again, sir. What brings you here today?",
+        "Sir, I hope your day is off to an excellent start. How may I assist you?",
+        "Welcome, master. Your presence always brightens the virtual halls.",
+        "Good to see you, sir. Is there a particular matter that requires attention?",
+        "Sir, may I extend my warmest greetings to you on this fine day?",
+        "Ah, the prodigal user returns! How may I serve you today, sir?",
+        "Greetings, sir. As always, I am at your disposal for any assistance you may require.",
+        "Master, your arrival is most welcome. How may I assist you at this moment?",
+    ];
 
-const userXP = {};
+    const mostWantedList = [
+        {
+            rank: '#1',
+            name: 'The Joker',
+            alias: 'Clown Prince of Crime',
+            description: 'Known for his chaotic and unpredictable nature. Approach with extreme caution.',
+        },
+        {
+            rank: '#2',
+            name: 'Two-Face',
+            alias: 'Harvey Dent',
+            description: 'Former district attorney turned criminal mastermind. Has a coin that determines his decisions.',
+        },
+        {
+            rank: '#3',
+            name: 'Bane',
+            alias: 'The Man Who Broke the Bat',
+            description: 'Enhanced strength and intelligence. Highly skilled in hand-to-hand combat.',
+        },
+        {
+            rank: '#4',
+            name: 'Harley Quinn',
+            alias: 'The Joker\'s Gal',
+            description: 'Former psychiatrist turned accomplice of The Joker. Skilled gymnast and proficient with various weapons.',
+        },
+        {
+            rank: '#5',
+            name: 'Poison Ivy',
+            alias: 'The Botanical Bio-Terrorist',
+            description: 'Possesses control over plants and toxins. Advocate for environmental protection, with a dangerous edge.',
+        },
+        {
+            rank: '#6',
+            name: 'Catwoman',
+            alias: 'The Feline Fatale',
+            description: 'Master thief with a complex relationship with Batman. Exceptional agility and combat skills.',
+        },
+        {
+            rank: '#7',
+            name: 'Mr. Freeze',
+            alias: 'The Cryonic Criminal',
+            description: 'Former scientist turned cryogenic criminal. Uses a freeze gun to create ice-based weaponry.',
+        },
+        {
+            rank: '#8',
+            name: 'Scarecrow',
+            alias: 'Master of Fear',
+            description: 'Psychiatrist turned criminal who uses fear-inducing toxins. Exploits the fears of his victims.',
+        },
+    ];
 
-client.once('ready', () => {
-    console.log('Bot is online!');
-});
-
-client.on('message', (message) => {
-    if (message.author.bot) return;
-
-    // Increment user XP
-    const userId = message.author.id;
-    userXP[userId] = (userXP[userId] || 0) + XP_PER_MESSAGE;
-
-    // Check for level-up
-    const userLevel = Math.floor(userXP[userId] / 100);
-    if (userLevel > 0) {
-        message.channel.send(`${message.author.username}, you leveled up to level ${userLevel}!`);
-    }
-
-    // Check for commands
-    if (message.content.startsWith(PREFIX)) {
-        const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-
-        // Add more commands if needed
-        if (command === 'level') {
-            message.channel.send(`You are currently level ${userLevel}.`);
+    const riddlesArray = [
+        "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?",
+        "The more you take, the more you leave behind. What am I?",
+        "What has keys but can't open locks?",
+        "I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?",
+        "The person who makes it, sells it. The person who buys it never uses it. What is it?",
+        "The more you cut me, the bigger I grow. What am I?",
+        "I have cities, but no houses. I have mountains, but no trees. I have water, but no fish. What am I?",
+        "I fly without wings. I cry without eyes. Wherever I go, darkness follows me. What am I?",
+        "I am always hungry. I must always be fed. The finger I touch will soon turn red. What am I?",
+        "What comes once in a minute, twice in a moment, but never in a thousand years?"
+      ];
+    
+    client.on('interactionCreate', async (interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+    
+        if (interaction.commandName === 'hey') {
+                const randomResponse = alfredGreetings[Math.floor(Math.random() * alfredGreetings.length)];
+                return interaction.reply(randomResponse);
         }
-    }
-});
+    
+        if (interaction.commandName === 'ping') {
+            return interaction.reply('Pong!');
+        }
 
-registerCommands(client);
+        if (interaction.commandName === 'website') {
+                return interaction.reply('https://oliverjsmith.xyz');
+        }
 
-client.on('message', (message) => {
-    if (message.author.bot) return;
+        if (interaction.commandName === 'jokerstatus') {
+                const randomNumber = Math.floor(Math.random() * 100);
 
-    // Increment user XP
-    const userId = message.author.id;
-    userXP[userId] = (userXP[userId] || 0) + XP_PER_MESSAGE;
+                if (randomNumber < 5) {
+                        return interaction.reply('The Joker is loose!');
+                } else {
+                        return interaction.reply('The Joker is in Arkham Asylum.');
+                }
+        }
 
-    // Check for level-up
-    const userLevel = Math.floor(userXP[userId] / 100);
-    if (userLevel > 0) {
-        message.channel.send(`${message.author.username}, you leveled up to level ${userLevel}!`);
-        // You can add additional actions when a user levels up
-    }
-});
+        if (interaction.commandName === 'gcpd') {
+                const gcpdEmbed = new EmbedBuilder()
+                    .setTitle('GCPD\'s Most Wanted List')
+                    .setDescription('A list of Gotham City\'s most notorious criminals.')
+                    .setColor('#FF0000');
+
+                    mostWantedList.forEach((criminal) => {
+                        gcpdEmbed.addFields(
+                          { name: `${criminal.rank} ${criminal.name} (${criminal.alias})`, value: `${criminal.description}` }
+                        );
+                      });
+                  
+                      await interaction.reply({ embeds: [gcpdEmbed] });
+                }
+                
+        if (interaction.commandName === 'riddle') {
+            const randomRiddle = riddlesArray[Math.floor(Math.random() * riddlesArray.length)];
+                      await interaction.reply(`🤔 Riddle me this: ${randomRiddle}`);
+                    
+        }
+    });
+
+    
 
 
 client.on('ready', (c) => {
-    console.log(`✅ ${c.user.username} is online`);
-})
+        console.log(`✅ ${c.user.tag} is online.`);
+
+        client.user.setActivity({
+                name: 'Lego Batman'
+        })
+});
 
 client.login(process.env.TOKEN);
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,45 +1,47 @@
 require('dotenv').config();
-const { REST, Routes, ApplicationCommandOptionType } = require('discord.js')
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
 
-function registerCommands(client) {
-  const PREFIX = '!';
-
-  client.on('message', (message) => {
-      if (message.author.bot || !message.content.startsWith(PREFIX)) return;
-
-      const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-      const command = args.shift().toLowerCase();
-
-      if (command === 'level') {
-          checkLevel(message);
-      }
-      // Add more commands here if needed
-  });
-}
-
-function checkLevel(message) {
-  const userId = message.author.id;
-  const userXP = userXP[userId] || 0;
-  const userLevel = Math.floor(userXP / 100);
-
-  message.channel.send(`${message.author.username}, you are currently level ${userLevel}.`);
-}
-
-module.exports = registerCommands;
+const commands = [
+  {
+    name: 'hey',
+    description: 'Greets the user',
+  },
+  {
+    name: 'ping',
+    description: 'Pong!',
+  },
+  {
+    name: 'website',
+    description: 'Provides a link to the website',
+  },
+  {
+    name: 'jokerstatus',
+    description: 'Checks whether the Joker is loose or not',
+  },
+  {
+    name: 'gcpd',
+    description: 'Shows the gcpd\'s most wanted list',
+  },
+  {
+    name: 'riddle',
+    description: 'Gives you a riddle to solve',
+  }
+];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-    
+
 (async () => {
   try {
     console.log('Registering slash commands...');
-    
+
     await rest.put(
-      Routes.applicationGuildCommands (
-        process.env. CLIENT_ID,
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
         process.env.GUILD_ID
       ),
       { body: commands }
     );
+
     console.log('Slash commands were registered successfully!');
   } catch (error) {
     console.log(`There was an error: ${error}`);
